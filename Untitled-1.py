696b69f1f4a83ffabda6f7e894fd51faa35cd5c1
Barnes <365818@mcpsmd.org>
2019-12-04 11:53:16 -0500

    finished product

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,7 +1,8 @@
 name = input("What is your name? ")
 age = int(input("How old are you? "))
-print("Hello, " + name + "!")
+town = input("Where do you live?")
+print("Hello, " + name + " from " + town + "!")
 if age >= 13 and age <= 19: 
     print("you are a teenager")
 elif age < 13: 
-    print("You are a child")
+    print("You are a child") 
---
+++ guessingGame.py
@@ -0,0 +1,20 @@
+# Write a small game which picks a random number, and has the user 
+#guess until they get it right. The game should give hints about the
+#user needs to guess lower or higher.
+import random 
+num = random.randint(1,10) 
+guess = int(input("What is your guess from 1-10?"))
+if guess == num:
+    print("Congratulations, youve guessed it!")
+if guess != num:
+   while guess != num:
+        print("try again!")
+        if guess > num:
+            print("try lower this time")
+        if guess < num:
+            print("try higher this time")
+        guess = int(input(""))
+if guess == num:
+    print("Congratulations, you've guessed it!")
+
+
